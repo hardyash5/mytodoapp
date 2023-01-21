4a08691ea3c4abc1f6e6060b242878f9027d2e75
hardyash5 <hardyash5@gmail.com>
2023-01-20 22:35:23 -0500

    added input fields for contact form

--- my-app/src/components/ContactForm.js
+++ my-app/src/components/ContactForm.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const ContactForm = () => {
-  return (
-    <div>ContactForm</div>
-  )
+export default class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="contact-container">
+      <form className="contact-form">
+        <input
+        className="fname"
+        type="text"
+        placeholder="First Name"
+        />
+        <input
+        className="lname"
+        type="text"
+        placeholder="Last Name"
+        />
+        <br/>
+        <input
+        type="text"
+        placeholder="Email"
+        />
+        
+        <input
+        className="comments"
+        type="text"
+        placeholder="Comments"
+        />
+        <br/>
+        <button className="submit" type="submit">Submit</button>
+      </form>
+      </div>
+    )
+  }
 }
-
-export default ContactForm
